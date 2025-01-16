aec62e09f93dbb8c4e1c09c248bd471bb9da3a4d
Arthur <arthur.queiroz08@aluno.ifce.edu.br>
2025-01-16 15:55:04 -0300

    Create a Handler to TCP server in WorkspaceHandler

--- workspacehandler.go
+++ workspacehandler.go
@@ -3,6 +3,8 @@ package main
 import (
 	"go.i3wm.org/i3/v4"
 	"log"
+	"net"
+	"strings"
 	"sync"
 )
 
@@ -23,11 +25,11 @@ func (wh *WorkspaceHandler) ListenEvents() {
 		if event.Change == "focus" {
 			mutex.Lock()
 			if event.Old.Name != "" {
-				if x == 0 {
+				if ignoreEvent == 0 {
 					wh.ds.PushOnBack(event.Old.Name)
 					log.Printf("[ %s ] - Workspace pushed to stack", event.Old.Name)
 				} else {
-					x--
+					ignoreEvent--
 				}
 			}
 			mutex.Unlock()
@@ -36,5 +38,56 @@ func (wh *WorkspaceHandler) ListenEvents() {
 
 	if err := subscription.Close(); err != nil {
 		log.Printf("Error closing subscription: %v", err)
+	}
+}
+
+var (
+	BACK  = "back"
+	FRONT = "front"
+)
+
+func (wh *WorkspaceHandler) Handle(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Printf("Error reading from connection: %v", err)
+		return
+	}
+
+	msg := strings.TrimSpace(string(buf[:n]))
+	switch msg {
+	case BACK:
+		wh.treatBackMessage()
+	case FRONT:
+		wh.treatFrontMessage()
+	}
+	_, err = conn.Write([]byte("Message received and processed"))
+}
+
+func (wh *WorkspaceHandler) treatBackMessage() {
+	namePtr := wh.ds.PopOnBackAndPutOnFront()
+	if namePtr == nil {
+		return
+	}
+	goToWorkspaceName(*namePtr)
+}
+
+func (wh *WorkspaceHandler) treatFrontMessage() {
+	// TODO: Implement this
+	//name := wh.ds.PopOnFrontAndPutOnBack()
+	//goToWorkspaceName(*name)
+}
+
+func goToWorkspaceName(name string) {
+	command, err := i3.RunCommand("workspace" + name)
+	if err != nil {
+		log.Printf("Error running command on i3: %v", err)
+		return
+	}
+	for _, c := range command {
+		if !c.Success {
+			log.Printf("Command returned unsuccessful message: %v", c)
+		}
 	}
 }
